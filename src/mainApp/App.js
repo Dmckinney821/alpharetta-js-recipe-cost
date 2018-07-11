672cf53ab0669f1f405fd73b081937175980af18
Dan McKinney <3dhdaniel@gmail.com>
2018-07-11 19:06:36 -0400

    second

--- src/mainApp/App.js
+++ src/mainApp/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import './App.css';
+import Textbox from '..components/testbox';
+import { recipes } from '../data/recipes';
 
 class App extends Component {
   render() {
+    const recipeElements = recipes.map((recipe) => {
+      return (
+        <div key={}>{recipe.name}</div>
+      )
+    })
     return (
       <div className="App">
         <header className="App-header">
@@ -11,6 +18,10 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
+
+          Hello World
+          <TextBox text='Hello WOrld' />
+
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
